b2ea30e0e8af538e18ba508b87dd26782ceb7ccf
gabrielalvesbr <gabrielalvesbr91@gmail.com>
2026-09-09 17:36:52 -0300

    feat: make update route functional

--- src/routes/update.js
+++ src/routes/update.js
@@ -1,5 +1,26 @@
 import app from "../app.js";
+import sql from "../config/database.js";
 
-app.put('/update', (req, res) => {
-    res.json({"message" : "route update"})
+app.put('/update/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const { name } = req.body
+
+    try {
+        await sql`
+            UPDATE users SET name = ${name} WHERE id = ${id}
+        `
+
+        console.log('User updated');
+
+        res.json(
+            {message: "User updated"}
+        );
+
+    } catch (error) {
+        console.log('update error: ', error)
+
+        res.status(500).json({
+            message: "Error updating user"
+        });
+    }
 })
