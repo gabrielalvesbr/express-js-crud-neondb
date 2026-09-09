ebad35ad52f752ed0c7614442fe33823b833a88d
gabrielalvesbr <gabrielalvesbr91@gmail.com>
2026-09-09 17:14:00 -0300

    feat: make read route functional

--- src/routes/read.js
+++ src/routes/read.js
@@ -1,5 +1,21 @@
 import app from "../app.js";
+import sql from "../config/database.js";
 
-app.get('/', (req, res) => {
-    res.json({"message" : "route read"})
+app.get('/', async (req, res) => {
+    
+    try {
+        const users = await sql`SELECT * FROM users`
+
+        console.log('users data')
+
+        res.json(users)
+
+    } catch (error) {
+        console.log('read error: ', error)
+
+        res.status(500).json({
+            message: "Error reading users"
+        });
+    } 
+
 })
